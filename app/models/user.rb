a0914980d2ced3c288daa0820d6a497e510b08ea
38385kobi <111612810+38385kobi@users.noreply.github.com>
2022-09-20 16:00:40 +0900

    バリデーションの訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/,allow_blank: true}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/,allow_blank: true}
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/,allow_blank: true}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/,allow_blank: true}
   validates :birthday, presence: true
 end
