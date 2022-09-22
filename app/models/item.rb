c8ee264b3abecb7e642d05dd46335d880cd6af83
38385kobi <111612810+38385kobi@users.noreply.github.com>
2022-09-22 12:51:54 +0900

    itemモデル単体テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
   validates :shipping_fee_id, presence: true, numericality: { other_than: 0, message: "can't be blank" }
   validates :prefecture_id, presence: true, numericality: { other_than: 0, message: "can't be blank" }
   validates :delivery_date_id, presence: true, numericality: { other_than: 0, message: "can't be blank" }
-  validates :price, presence: true
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/,allow_blank: true}, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999,allow_blank: true}
   validates :image, presence: true
 
 end
