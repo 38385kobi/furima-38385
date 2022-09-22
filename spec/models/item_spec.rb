67fe0794e645c9ae5dc55507b35a87dfff712657
38385kobi <111612810+38385kobi@users.noreply.github.com>
2022-09-22 15:27:05 +0900

    単体モデルのexampleの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe Item, type: :model do
     @item = FactoryBot.build(:item)
   end
 
-  describe "出品" do
-    context "全ての情報が正しく入力されていれば" do
-      it "出品できる" do
+  describe "商品出品" do
+    context "商品登録ができる時" do
+      it "全ての情報が正しく入力されていれば出品できる" do
         expect(@item).to be_valid
       end
     end
-    context "imageが空だと" do
-      it "出品できない" do
+    context "商品登録ができない時" do
+      it "imageが空だと出品できない" do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
