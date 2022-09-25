27d9e2f7c3fee2af4e0aee6c7ccda15ad6b8befa
38385kobi <111612810+38385kobi@users.noreply.github.com>
2022-09-25 15:19:09 +0900

    環境変数の設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,9 +1,9 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_833b348abb76619548221fb2");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    
+
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
 
